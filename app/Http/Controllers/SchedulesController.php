ae106b8cddf2e1800bb4e66514e231d548963bdc
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2016-02-16 02:24:06 +0700

    schedule api /index

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -11,8 +11,16 @@ use App\Http\Controllers\Controller;
 
 class SchedulesController extends Controller {
 
+	public function index($roomId) {
+    	$schedules = Schedule::with(['course'])
+    		->Where('room_id', $roomId)
+    		->orderBy('start', 'asc')
+    		->get();
+
+    	return response()->json($schedules);
+    }
+
     public function show($roomId, $date) {
-    	// date parse
     	$objectDate = \DateTime::createFromFormat('m-d-Y', $date);
     	$filterDate = $objectDate->format("Y/m/d");
 
@@ -23,25 +31,5 @@ class SchedulesController extends Controller {
     		->get();
 
     	return response()->json($schedules);
-    	// if ($roomId == 1)
-	    //     return response()->json(array(
-		   //      'error' => false,
-		   //      'schedules' => [
-		   //      		array("start"=>1, "duration"=>3, "courseName" => "IF3230"),
-		   //      		array("start"=>4, "duration"=>2, "courseName" => "IF3250"),
-		   //      		array("start"=>6, "duration"=>1, "courseName" => "IF3240"),
-		   //      	]),
-		   //      200
-		   //  );
-	    // else
-	    // 	return response()->json(array(
-		   //      'error' => false,
-		   //      'schedules' => [
-		   //      		array("start"=>1, "duration"=>2, "courseName" => "IF2130"),
-		   //      		array("start"=>3, "duration"=>3, "courseName" => "IF2150"),
-		   //      		array("start"=>6, "duration"=>2, "courseName" => "IF3110"),
-		   //      	]),
-		   //      200
-		   //  );
     }
 }
